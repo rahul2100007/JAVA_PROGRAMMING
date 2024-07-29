574379817536a3d7f1ee37cd539ae59a93b2e49a
Rahul Prajapati <rahulprajapati23@gnu.ac.in>
2024-07-29 18:00:01 +0530

    edit code

--- hello_world.java
+++ hello_world.java
@@ -1,4 +1,4 @@
-public class hello_world {
+class hello_world {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hello rahul :)");
