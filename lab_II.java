60205651fec70804edc9dc131ec33cff031d69ef
Rahul Prajapati <rahulprajapati23@gnu.ac.in>
2024-07-31 17:59:14 +0530

    add file

---
+++ lab_II.java
@@ -0,0 +1,75 @@
+class lab_II
+{
+    public static void main(String args[])
+    {
+        //==========================TYPE CASTING===========================//
+
+
+        System.out.println("\nINT TO CHAR");
+        int Z=92;
+        char C = (char)Z;
+        System.out.println(C);
+
+        System.out.println("\nCHAR TO INT");
+        char chr= 'a' ;
+        int Cn = (int)chr;
+        System.out.println(Cn);
+
+        System.out.println("\nINT TO FLOAT");
+        int A = 20;
+        float F = A;
+        System.out.println(F);
+
+        System.out.println("\nINT TO LONG");
+        int Il = 90;
+        long Cv = Il;
+        System.out.println(Cv);
+
+        System.out.println("\nINT TO DOUBLE");
+        int Id = 40;
+        long Dx = Id;
+        System.out.println(Dx);
+
+        System.out.println("\nFLOAT TO DOUBLE");
+        float B = 20.30f;
+        double D = B;
+        System.out.println(D);
+
+        System.out.println("\nLONG TO DOUBLE");
+        long Ln = 208470l;
+        double Db = Ln;
+        System.out.println(Db);
+
+        System.out.println("\nLONG TO FLOAT");
+        long L = 67570l;
+        float Dn = L;
+        System.out.println(Dn);
+
+        System.out.println("\nLONG TO INT");
+        long Li = 6394738454L;
+        int Di = (int) Li;
+        System.out.println(Di);
+
+        System.out.println("\nFLOAT TO INT");
+        float Fd = 20.30f;
+        int Ab = (int) Fd;
+        System.out.println(Ab);
+
+        System.out.println("\nDOUBLE TO FLOAT");
+        double Da = 8.54689579;
+        float Fl = (float) Da;
+        System.out.println(Fl);
+
+        System.out.println("\nDOUBLE TO INT");
+        double Dti = 8.54689579;
+        int Nu = (int) Dti;
+        System.out.println(Nu);
+		
+		
+		/*
+		String name = "Rahul";
+		System.out.println(name+70+70);
+		*/
+
+    }
+}
