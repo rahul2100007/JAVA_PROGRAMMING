ea8fb0d80316712a6eb7f9bb664a11509a70172c
Rahul Prajapati <rahulprajapati23@gnu.ac.in>
2024-08-08 08:22:59 +0530

    add file

---
+++ pattern.java
@@ -0,0 +1,11 @@
+class Pattern {
+    public static void main(String[] args) {
+        System.out.println("       __       ___      ____    ____      ___      ");
+        System.out.println("      |  |     /   \\     \\   \\  /   /     /   \\     ");
+        System.out.println("      |  |    /  ^  \\     \\   \\/   /     /  ^  \\    ");
+        System.out.println(".--.  |  |   /  /_\\  \\     \\      /     /  /_\\  \\   ");
+        System.out.println("|  `--'  |  /  _____  \\     \\    /     /  _____  \\  ");
+        System.out.println(" \\______/  /__/     \\__\\     \\__/     /__/     \\__\\ ");
+        System.out.println("                                                    ");
+    }
+}
